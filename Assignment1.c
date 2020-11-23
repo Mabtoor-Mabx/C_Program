d8604d4a5f9e05cf4019743ed1ad95922d99ddbd
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-23 22:58:36 +0500

    Add files via upload
    First Program Of C Language

---
+++ Assignment1.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+
+// Problem 1
+
+// int length =10;
+// int width = 20;
+// int Area = length * width;
+
+// printf("Area Of Triangle is %d" , Area);
+
+// int a,b;
+
+// printf("Enter Length \n");
+// scanf("%d", &a);
+// printf("Enter Width \n ");
+// scanf("%d", &b);
+
+// int Area = a * b;
+
+// printf("The Area Of Triangle is %d" , Area);
+
+
+
+
+
+// Problem 2
+
+// int r, h;
+// printf("Enter Radius value \n");
+// scanf("%d", &r);
+// float area = 3.14 * r *r;
+// printf("The Area Of circle Is %f" , area);
+// printf("\n");
+// printf("Enter Height Value \n");
+// scanf("%d", &h);
+// float cyliner = (2 * 3.14 * r * h) + (2 * r * r);
+// printf("The Volume Of Cyliner Is %f", cyliner );
+
+
+
+// Problem 3
+
+
+// float c;
+
+// printf("Enter Centigrade value \n");
+// scanf("%f", &c);
+
+// float Farenheight = ( c * 9 / 5)   + 32;
+// printf("The Temprature In Farenheight is %f" , Farenheight); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+
+}
---
+++ FirstProgram.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+//     int a = 10;
+//     int b = 20;
+//     int c = a+b;
+//     printf("The Sum of Value is %d", c);
+//     return 0;
+//
+
+// int no ;
+
+// scanf("%d", &no);
+
+// printf( "%d", no);
+
+// return 0;
+
+
+
+
+
+
+
+}
