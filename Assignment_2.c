dd222513d74860f63df233f9bbb5c9eef1e039dc
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-26 21:29:36 +0500

    Add files via upload
    Done

---
+++ Assignment_2.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+
+  int main() {
+
+ // Problem 1
+    // int a ; b=a;
+    // int v = 3^3;
+    // char dt = '21 dec 2020';
+            
+    // printf(a,v,dt);
+
+
+    // Problem 2
+
+
+        // float d = 3.0/8-2;
+        // printf("%f", d);
+
+
+            // Problem 3
+
+int a;
+printf("Enter A Number \n");
+scanf("%d", & a);  
+printf("The Remainder After Divisible by 97 is %d \n :" , a%97);   
+
+
+
+     return 0;
+}
