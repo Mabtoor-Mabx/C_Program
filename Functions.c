2ba7856666eab6105def11db8edefc5f16a44af2
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-12-09 21:44:07 +0500

    Add files via upload
    Functions in C

---
+++ Functions.c
@@ -0,0 +1,29 @@
+
+// Quick Quiz
+
+#include<stdio.h> 
+
+void GoodMorning();
+void GoodAfternoon();
+void GoodNight();
+  int main() {
+
+GoodMorning();
+GoodAfternoon();
+GoodNight();
+     return 0;
+}
+
+void GoodMorning()
+{
+    printf("Good Morning ! Have A Nice day \n");
+}
+
+void GoodAfternoon()
+{
+    printf("Good AfterNoon! Stay Blessed! \n");
+}
+void GoodNight()
+{
+    printf("Good Night! Sweet Dreams \n ");
+}
